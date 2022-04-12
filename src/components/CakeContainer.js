9b2a5cfef22434959bad4854ad1c4fd948933a5e
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-04-12 21:23:33 +0600

    cake shop redux

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CakeContainer = () => {
+    return (
+        <div>
+            <h1>Number Of cakes {0}</h1>
+            <button>Buy Cakes</button>
+            
+        </div>
+    );
+};
+
+export default CakeContainer;
---
+++ src/redux/cake/cakeAction.js
@@ -0,0 +1,10 @@
+import { BUY_CAKE } from './cakeTypes' 
+
+export const buyCake = (number = 1)=> {
+
+    return {
+        type: BUY_CAKE,
+        payload:number
+
+    }
+}
