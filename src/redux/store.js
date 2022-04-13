31d187190d7940702fa6d83352a686ee2bf86e80
“shafikulislam067@gmail.comgit config --global user.name “shafikul islam <shafikulislam067@gmail.comgit config --global user.email shafikulislam067@gmail.com>
2022-04-13 11:29:05 +0600

    redux dev tool add

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './rootReducer';
-const store = createStore(rootReducer);
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
+
+
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 export default store;
